12325f2d20970878fc636ec75abb50d6173b794c
gohar94 <goharirfan94@gmail.com>
2016-03-28 09:31:22 +0500

    brew installations

---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+
+
+# Some important tools
+brew install cmake
+brew install cscope
+brew install ctags
+brew install gdb
+brew install autoconf
+brew install automake
+
+
+# For installing GCC 4.9
+brew tap homebrew/versions
+brew install gcc49
+
+# Remove outdated versions from the cellar
+brew cleanup
